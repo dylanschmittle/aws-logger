3964c6030a7532985a5cb125fa76a89cb34be8d2
dylanschmittle <da.schmittle89@gmail.com>
2019-10-03 17:07:15 -0400

    Add files via upload
    Aws Wrapper

---
+++ EzAws.py
@@ -0,0 +1,13 @@
+#AWS Wrapper Class 
+class EzAws:
+    s3bucket = S3_BUCKET
+    def upload_file(self, file_name, __bucket, object_name=None):
+        if object_name is None:
+            object_name = file_name
+        s3_client = boto3.client('s3')
+        try:
+            response = s3_client.upload_file(file_name, bucket, object_name)
+        except ClientError as e:
+            logging.error(e)
+            return(False)
+        return(True)
