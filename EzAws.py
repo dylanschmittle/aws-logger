b53b1a75035f7b099ce048eeccc7494a0a86fd8c
Dylan Schmittle <da.schmittle89@gmail.com>
2019-10-04 01:56:25 -0400

    Readme Updates and Cleanup

--- EzAws.py
+++ EzAws.py
@@ -22,9 +22,15 @@ class EzAws():
         """
         if object_name is None:
             object_name = file_name
+
         s3_client = boto3.client('s3')
+
         try:
             response = s3_client.upload_file(file_name, bucket, object_name)
+
         except ClientError as e:
             logging.error(e)
+
         return response
+
+    def 
