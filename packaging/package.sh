b6f843147db484274586b30c74362ab8cfcd23b0
Dylan Schmittle <da.schmittle89@gmail.com>
2019-10-08 17:07:18 -0400

    lambda stuff

---
+++ packaging/package.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export PKG_DIR="python"
+
+rm -rf ${PKG_DIR} && mkdir -p ${PKG_DIR}
+
+docker run --rm -v $(pwd):/foo -w /foo lambci/lambda:build-python3.6 \
+    pip install -r requirements.txt --no-deps -t ${PKG_DIR}
+
+zip -r pymogo-boto3-python36.zip .
