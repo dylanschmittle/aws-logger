c18dceea87281aa5cc8f5c63a17b3a993e614abe
Dylan Schmittle <da.schmittle89@gmail.com>
2019-10-09 15:39:56 -0400

    updating layer script and gitignore

--- lambda_function.py
+++ lambda_function.py
@@ -9,26 +9,40 @@ def lambda_handler(event, context):
     # if event is timed event, throw the logs from the last
     # time till now at the mELK stack
     # Connections Are Opening
-    groups = ["Ci", "Beta"]
-    mongo_uri = "#ENV IN LAMBDA"
-    s3_name = "#ENV IN LAMBDA"
-    conn = LogLogger(mongo_uri, s3_name, groups)
-    # Send ENV for Groups to grab into this call, iterate
-    # through list of groups
-    document = conn.fetch()
-    print(json.dumps(document))
-    transfer = conn.put_mongo()
-    print(json.dumps(transfer))
-    s3transfer = conn.put_s3()
-    print(json.dumps(s3transfer))
+    # groups = ["Ci", "Beta"]
+    # mongo_uri = "#ENV IN LAMBDA"
+    # s3_name = "#ENV IN LAMBDA"
+    # conn = LogLogger(mongo_uri, s3_name, groups)
+    # # Send ENV for Groups to grab into this call, iterate
+    # # through list of groups
+    # document = conn.fetch()
+    # print(json.dumps(document))
+    # transfer = conn.put_mongo()
+    # print(json.dumps(transfer))
+    # s3transfer = conn.put_s3()
+    # print(json.dumps(s3transfer))
 
-    if(conn.hasFailed):
-        return {
-            'statusCode': 500,
-            'body': conn.dumps()
-            }
+    # if(conn.hasFailed):
+    #     return {
+    #         'statusCode': 500,
+    #         'body': conn.dumps()
+    #         }
+
+    # return {
+    #     'statusCode': 200,
+    #     'body': 'Log logging sucessful'
+    #     }
+    uri = "mongodb+srv://dylan:lVA51KGhSXxoAgW1@cluster0-fipww.gcp.mongodb.net/test?retryWrites=true&w=majority"
+    s3 = "mongo-db-overflow-log-failover"
+    testlog = WatchCloudLogs(uri, s3)
+    document = testlog.fetch()
+    # print(document)
+    # print("**Sending To DB")
+    response = testlog.put_mongo()
+    # print(response)
+
 
     return {
         'statusCode': 200,
-        'body': 'Log logging sucessful'
-        }
+        'body': "Win"
+    }
--- package.sh
+++ package.sh
@@ -5,6 +5,7 @@ export PKG_DIR="python"
 rm -rf ${PKG_DIR} && mkdir -p ${PKG_DIR}
 
 docker run --rm -v $(pwd):/foo -w /foo lambci/lambda:build-python3.6 \
-    pip install -r requirements.txt --no-deps -t ${PKG_DIR}
+    pip install -r requirements.txt --no-deps -t ${PKG_DIR} \
+    && pip install . --no-deps -t ${PKG_DIR}
 
 zip -r pymogo-boto3-python36.zip .
