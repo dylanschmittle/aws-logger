be098b71cb38a280d97ee2294a500c4548b1e483
Dylan Schmittle <da.schmittle89@gmail.com>
2019-10-10 01:27:39 -0400

    cleanup

--- setup-shard.sh
+++ setup-shard.sh
@@ -4,4 +4,5 @@ docker exec -it mongocfg1 bash -c "echo 'rs.initiate({_id: \"mongors1conf\",conf
 docker exec -it mongors1n1 bash -c "echo 'rs.initiate({_id : \"mongors1\", members: [{ _id : 0, host : \"mongors1n1\" },{ _id : 1, host : \"mongors1n2\" },{ _id : 2, host : \"mongors1n3\" }]})' | mongo"
 # Third, we introduce the shards to the router
 docker exec -it mongos1 bash -c "echo 'sh.addShard(\"mongors1/mongors1n1\")' | mongo "
-
+# Then Create the DB
+sh create-test-db.sh
