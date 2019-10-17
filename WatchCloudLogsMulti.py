9151c1438b35896dc24e1f0bd1d12c34671676c1
Dylan Schmittle <da.schmittle89@gmail.com>
2019-10-16 22:55:12 -0400

    testing multiprocessing for dockerlogs

--- WatchCloudLogsMulti.py
+++ WatchCloudLogsMulti.py
@@ -3,6 +3,7 @@ import boto3
 import json
 import logging
 import time
+import multiprocessing
 
 from botocore.exceptions import ClientError
 
@@ -84,9 +85,13 @@ class WatchCloudLogs():
         """
         # These two and nested asyncio
         # print(self.__logGroups)
+        p_group = list()
         for x in self.__logGroups:
-            print("Fetching Log Group: " + x)
-            self.put_group(x)
+            p = multiprocessing.Process(target=self.put_group(), args=(x))
+            p.start()
+            p_group.append(p)
+            print("Fetching Log Group Multiprocessing For Group: " + x)
+            # self.put_group(x)
         self.put_serverless(self)
         # Wait for the above and then squash
         # This is to possibly remove things we dont care to log
@@ -322,3 +327,59 @@ class WatchCloudLogs():
             'exception': e.dumps,
         }
         return e_log
+
+
+if __name__ == "__main__":
+    """Simple Testing Method
+    Tests:
+        Initizile
+        Change DB
+        Get Single Strean
+        Get all a Log Group's Streams
+        Get Multiple Groups
+        Error Handle Connection Issues
+        Internal Error Log w/ Python
+        Send To s3
+        Squashing Duplicates
+    """
+    # Assume Lambda Has Permissions and Access to Resources, or local CLI
+    # tool is configured and has Permissions
+    uri = "mongodb+srv://dylan:lVA51KGhSXxoAgW1@cluster0-fipww.gcp.mongodb.net/test?retryWrites=true&w=majority"
+    uri_badcreds = "mongodb+srv://dyln:lVA66666hSXxoAgW1@cluster0-fipww.gcp.mongodb.net/test?retryWrites=true&w=majority"
+    uri_fake = "mongodb+srv://lan:lVA51KGAgW1@cluster0-fipww.gcp.mo.net/test?retryWrites=true&w=majority"
+    uri_invalid = "mongoERRdb+srv://user:lVA5zzhSXxoAgW1@fake.mongodb.net/fake?out"
+    s3 = "mongo-db-overflow-log-backup"
+    s3_dne = "mongo-db-nonexistant-s3"
+    print("===========START __init__ & dumps TEST========")
+    print("(1) Constructor Passed URI and s3")
+    testlog = WatchCloudLogs(uri, s3)
+    variabledict = testlog.dumps()
+    print(variabledict)
+    # testlogInvalid = WatchCloudLogs()
+    # variabledictInvalid = testlogInvalid.dumps()
+    # print("(2) Default Constructor")
+    # print(variabledictInvalid)
+    print("===========END TEST=======================")
+    print("===========START MONGO URI TEST===========")
+    # print("(1) Invalid URI")
+    # response = testlog.change_uri(uri_invalid)
+    # print(response)
+    # print("(2) Invalid DNS")
+    # response = testlog.change_uri(uri_fake)
+    # print(response)
+    # print("(3) Invalid Creds")
+    # response = testlog.change_uri(uri_badcreds)
+    # print(response)
+    print("(4) Valid Creds")
+    response = testlog.change_uri(uri)
+    print(response)
+    print("==========END MONGO URI TEST==============")
+
+    print("========START DEFAULT STREAM TEST==========")
+    testlog = WatchCloudLogs(uri, s3)
+    document = testlog.fetch()
+    # print(document)
+    print("**Sending To DB")
+    response = testlog.put_mongo()
+    print(response)
+    print("========END DEFAULT STREAM TEST============")
